7fdab18e837ad99771044f7090f59a51ee700929
Arnold Szederjesi <szederjesiarnold@gmail.com>
2020-05-07 13:38:09 +0300

    Take port from env variables, fallback to 8080

--- backend/web/web.go
+++ backend/web/web.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -18,14 +20,20 @@ func StartWebServer() {
 	spa := SPAHandler{StaticPath: "build", IndexPath: "index.html"}
 	router.PathPrefix("/").Handler(spa)
 
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		logrus.Warn("Could not get port from env variables, falling back to 8080")
+		port = 8080
+	}
+
 	srv := &http.Server{
 		Handler: router,
-		Addr:    "0.0.0.0:8080",
+		Addr:    "0.0.0.0:" + strconv.Itoa(port),
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	logrus.Info("Listening on port 8080...")
+	logrus.Info("Listening on port " + strconv.Itoa(port))
 	logrus.Fatal(srv.ListenAndServe())
 }
