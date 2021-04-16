dc5bef4445146956b475897c192a6890b83d261e
Arnold Szederjesi <szederjesiarnold@gmail.com>
2021-04-16 13:48:07 +0300

    Add Elixir and OTP versions

--- backend/mix.exs
+++ backend/mix.exs
@@ -5,7 +5,7 @@ defmodule Backend.MixProject do
     [
       app: :backend,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :rustler] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
